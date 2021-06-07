9abc5835c0465b7c782d11ae598756958d04041c
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-06-07 13:42:39 -0300

    firts

--- start/routes.ts
+++ start/routes.ts
@@ -20,5 +20,7 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.post('/users', 'UsersController.create')
-Route.get('/users', 'UsersController.index')
+Route.group(() => {
+  Route.post('/users', 'UsersController.create')
+  Route.get('/users', 'UsersController.index')
+}).prefix('/api')
